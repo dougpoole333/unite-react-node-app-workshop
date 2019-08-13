1c94f6d4a3ce7e19c93931b2f8caecb37c259deb
User one <userone@Users-Mac-mini.lan1>
2019-08-13 16:06:19 -0400

    built thing

--- server/index.js
+++ server/index.js
@@ -1,13 +1,46 @@
 import dotenv from 'dotenv';
 import Koa from 'koa';
+import session from 'koa-session';
+import createShopifyAuth, {
+  createVerifyRequest,
+} from '@shopify/koa-shopify-auth';
+import webpack from 'koa-webpack';
+import renderReactApp from './render-react-app'
+import graphQLProxy from '@shopify/koa-shopify-graphql-proxy';
 
 dotenv.config();
+const {SHOPIFY_SECRET, SHOPIFY_API_KEY} = process.env;
 
 const app = new Koa();
+app.use(session(app));
 
-app.use(function index(ctx) {
-  console.log('Hello Unite 👋')
-  ctx.body = 'Hello Unite 👋';
-});
+app.keys = [SHOPIFY_SECRET];
+
+app.use(
+  createShopifyAuth({
+    // your shopify app's api key
+    apiKey: SHOPIFY_API_KEY,
+    // your shopify app's api secret
+    secret: SHOPIFY_SECRET,
+    // our app's permissions
+    // we need to write products to the user's store
+    scopes: ['write_products'],
+    // our own custom logic after authentication has completed
+    afterAuth(ctx) {
+      const {shop, accessToken} = ctx.session;
+
+      console.log('We did it!', shop, accessToken);
+
+      ctx.redirect('/');
+    },
+  }),
+);
+app.use(graphQLProxy)
+
+app.use(createVerifyRequest());
+
+app.use(webpack());
+
+app.use(renderReactApp);
 
 export default app;
